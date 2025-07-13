f6a2c4844ea54868c78016155713b320a608f14f
Abdallah <abdallaahnoor@gmail.com>
2025-07-13 18:31:28 +0300

    fix: correct typo in className for SectionTag component

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -43,7 +43,7 @@ export default function SectionHeader({
         <SectionTag
           containerClassName="rounded-full"
           as="div"
-          className="section-teg text-foreground"
+          className="section-teg text-foreground!"
         >
           <span>{tag}</span>
         </SectionTag>
