97ff1cda02be7a4f5de1a632518cbaf89af02139
Abdallah <abdallaahnoor@gmail.com>
2025-07-14 20:55:02 +0300

    refactor: remove About section component

--- sections/About.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <section id="about" className="flex h-screen items-center justify-center">
-      <h1 className="text-7xl font-bold">About</h1>
-    </section>
-  );
-}
